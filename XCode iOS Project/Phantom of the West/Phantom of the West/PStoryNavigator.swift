059ae0ea3a3e5f5f454d16c369e02566705ce2e7
Christopher Raleigh <craleigh318@comcast.net>
2015-07-17 03:01:34 -0700

    More rewrite.

---
+++ XCode iOS Project/Phantom of the West/Phantom of the West/PStoryNavigator.swift
@@ -0,0 +1,35 @@
+//
+//  PStoryNavigator.swift
+//  Phantom of the West
+//
+//  Created by Christopher Raleigh on 2015/17/07.
+//  Copyright (c) 2015 Christopher Raleigh. All rights reserved.
+//
+
+/*
+Handles events for the StoryNavigator buttons.
+*/
+protocol PStoryNavigator {
+    /*
+    The continue button is pressed.
+    */
+    func onButtonContinue()
+    
+    /*
+    The previous button is pressed.
+    */
+    func onButtonPrevious()
+    
+    /*
+    The next button is pressed.
+    */
+    func onButtonNext()
+    
+    /*
+    The options button is pressed.
+    */
+    func onButtonOptions()
+}
+
+
+
---
+++ XCode iOS Project/Phantom of the West/Phantom of the West/PStoryObserver.swift
@@ -0,0 +1,18 @@
+//
+//  PStoryObserver.swift
+//  Phantom of the West
+//
+//  Created by Christopher Raleigh on 2015/17/07.
+//  Copyright (c) 2015 Christopher Raleigh. All rights reserved.
+//
+
+/*
+A user interface that displays a story.
+*/
+public protocol PStoryObserver {
+    
+    /*
+    Notifies this observer that a new collection of pages is set.
+    */
+    func update([PStoryPage]);
+}
