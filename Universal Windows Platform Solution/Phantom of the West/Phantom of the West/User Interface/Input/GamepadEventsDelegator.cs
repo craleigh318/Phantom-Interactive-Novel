d864f1eaadb894b127af0cd22de0c2db9598fe95
Christopher Raleigh <craleigh318@comcast.net>
2015-11-25 17:24:53 -0800

    Started gamepad support.

---
+++ Universal Windows Platform Solution/Phantom of the West/Phantom of the West/User Interface/Input/GamepadEventsDelegator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Gaming.Input;
+
+namespace Phantom_of_the_West.User_Interface.Input
+{
+	public class GamepadEventsDelegator
+	{
+		public void ReadGamepad(Gamepad g, IGamepadEvents delegatee)
+		{
+			GamepadReading r = g.GetCurrentReading();
+			// Was a button pressed?
+			GamepadButtons b = r.Buttons;
+			if (InputTriggered(b))
+			{
+				delegatee.GamepadButtonPressed(b);
+			}
+			// Did the left stick move?
+			GamepadButtons ls = AnalogueDirectionToDigital(r);
+			if (InputTriggered(ls))
+			{
+				delegatee.LeftThumbstickMoved(ls);
+			}
+		}
+
+		private bool InputTriggered(GamepadButtons b)
+		{
+			return (b != GamepadButtons.None);
+		}
+
+		private GamepadButtons AnalogueDirectionToDigital(GamepadReading r)
+		{
+			double x = r.LeftThumbstickX;
+			double y = r.LeftThumbstickY;
+			Dictionary<double, GamepadButtons> mappings = new Dictionary<double, GamepadButtons>()
+				{
+				{x, GamepadButtons.DPadRight},
+				{-x, GamepadButtons.DPadLeft},
+				{y, GamepadButtons.DPadDown},
+				{-y, GamepadButtons.DPadUp},
+			};
+			foreach (KeyValuePair<double, GamepadButtons> p in mappings)
+			{
+				if (AnaloguePressed(p.Key))
+				{
+					return p.Value;
+				}
+			}
+			return GamepadButtons.None;
+		}
+
+		private bool AnaloguePressed(double position)
+		{
+			return (position >= 0.5);
+		}
+	}
+}
---
+++ Universal Windows Platform Solution/Phantom of the West/Phantom of the West/User Interface/Input/IGamepadEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Gaming.Input;
+
+namespace Phantom_of_the_West.User_Interface.Input
+{
+	public interface IGamepadEvents
+	{
+		void GamepadButtonPressed(GamepadButtons b);
+
+		void LeftThumbstickMoved(GamepadButtons b);
+	}
+}
