4626b337c39696af14c48a1a041c4e3589816607
Christopher Raleigh <craleigh318@comcast.net>
2016-01-20 19:52:13 -0800

    Removed old files.

--- XCode Mac Project/Phantom of the West/Phantom of the West/AppDelegate.swift
+++ XCode Mac Project/Phantom of the West/Phantom of the West/AppDelegate.swift
@@ -10,18 +10,61 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    static var potwStory: PhantomOfTheWest = PhantomOfTheWest()
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        startUp()
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func startUp() {
+        
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        rootSubcontroller = viewControllers[0] as? StoryViewController
+        if let subcontroller = rootSubcontroller {
+            subcontroller.storyViewHandler = self
+            
+            //Start story.
+            let story = AppDelegate.potwStory
+            story.observer = subcontroller
+            if let autoSave = AutoSave.data {
+                story.loadGame(autoSave)
+            } else {
+                story.playTutorial()
+            }
+        }
+        let localPlayer = GKLocalPlayer.localPlayer()
+        if !localPlayer.authenticated {
+            ManualSave.authenticatePlayer()
+        }
+    }
+    
+    func onButtonContinue() {
+        if let subcontroller = rootSubcontroller {
+            subcontroller.onButtonContinue()
+        }
+    }
+    
+    func onButtonPrevious() {
+        if let subcontroller = rootSubcontroller {
+            subcontroller.onButtonPrevious()
+        }
+    }
+    
+    func onButtonNext() {
+        if let subcontroller = rootSubcontroller {
+            subcontroller.onButtonNext()
+        }
+    }
 }
-
