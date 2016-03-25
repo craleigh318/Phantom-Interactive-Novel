77d26ff21750d5fe79ee27d552e7c0005fe77e0d
Christopher Raleigh <craleigh318@comcast.net>
2016-03-24 17:39:59 -0700

    Set image view height.

--- Android Project/app/src/main/java/com/christopherraleigh/phantomofthewest/MainActivity.java
+++ Android Project/app/src/main/java/com/christopherraleigh/phantomofthewest/MainActivity.java
@@ -1,13 +1,26 @@
 package com.christopherraleigh.phantomofthewest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final float IMAGE_VIEW_ASPECT_RATIO = 16 / 9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setImageViewHeight();
+    }
+
+    private void setImageViewHeight() {
+        View iv = findViewById(R.id.imageView);
+        float ivWidth = (float) iv.getWidth();
+        float newHeight = ivWidth / IMAGE_VIEW_ASPECT_RATIO;
+        int newHeightInt = Math.round(newHeight);
+        iv.requestLayout();
+        iv.getLayoutParams().height = newHeightInt;
     }
 }
