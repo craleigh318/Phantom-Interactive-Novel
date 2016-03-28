91eebb6918b9d9c710d0c9d549cb7bbdb0f3cdf7
Christopher Raleigh <craleigh318@comcast.net>
2016-03-27 21:01:35 -0700

    Image fills width.

--- Android Project/app/src/main/java/com/christopherraleigh/phantomofthewest/MainActivity.java
+++ Android Project/app/src/main/java/com/christopherraleigh/phantomofthewest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.christopherraleigh.phantomofthewest;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,15 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setImageViewHeight();
     }
 
-    private void setImageViewHeight() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //setImageViewHeight();
+    }
+
+    private void aliasImageView() {
         View iv = findViewById(R.id.imageView);
+        //BitmapDrawable draw = new BitmapDrawable(iv.getsr);
+        draw.setAntiAlias(false);
+        imageView.setImageDrawable(draw);
+
+        iv.requestLayout();
         float ivWidth = (float) iv.getWidth();
         float newHeight = ivWidth / IMAGE_VIEW_ASPECT_RATIO;
         int newHeightInt = Math.round(newHeight);
-        iv.requestLayout();
         iv.getLayoutParams().height = newHeightInt;
     }
 }
