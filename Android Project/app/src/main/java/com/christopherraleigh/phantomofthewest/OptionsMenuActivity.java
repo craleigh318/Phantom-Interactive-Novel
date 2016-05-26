87f5f83c44b9fdba201aedc0f1b3b65335096682
Christopher Raleigh <craleigh318@comcast.net>
2016-05-26 16:52:01 -0700

    Added voiceover preference.

--- Android Project/app/src/main/java/com/christopherraleigh/phantomofthewest/OptionsMenuActivity.java
+++ Android Project/app/src/main/java/com/christopherraleigh/phantomofthewest/OptionsMenuActivity.java
@@ -1,9 +1,9 @@
 package com.christopherraleigh.phantomofthewest;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public class OptionsMenuActivity extends PreferenceActivity {
+public class OptionsMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
